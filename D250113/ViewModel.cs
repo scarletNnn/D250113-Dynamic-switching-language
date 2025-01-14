c486914ffbdaaee2ed55b73e937b26fd542866db
夜 绯 <1012554927@qq.com>
2025-01-14 15:53:02 +0800

    添加项目文件。

---
+++ D250113/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace D250113;
+
+public partial class ViewModel : ObservableObject
+{
+    [ObservableProperty]
+    enum_DiagonaLift enum_DiagonaLift;
+}
---
+++ MyClassLibrary/MyLibrary.cs
@@ -0,0 +1,12 @@
+﻿using MyClassLibrary.Resources;
+
+namespace MyClassLibrary
+{
+    public class MyLibrary
+    {
+        public static string GetMessage()
+        {
+            return Resource1.Message;
+        }
+    }
+}
